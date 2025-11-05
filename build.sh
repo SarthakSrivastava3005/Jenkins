1effd63305664d327ec6d7eaf73dd30ecaff39c1
Sarthak Srivastava <54740977+Sarthak-Srivastava03@users.noreply.github.com>
2025-11-05 17:33:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Hello from Jenkins build job with WebHook configured!"
+echo "Now, whenever you push a commit to GitHub, Jenkins will automatically:
+Receive webhook →
+Pull latest code →
+Trigger a build 🎯"
 date
